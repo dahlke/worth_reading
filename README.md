cb538f29f448ff5d0506a64542f0eb7a1d74b16e
Neil Dahlke <neil.dahlke@gmail.com>
2021-01-08 09:43:33 -0800

    Adding some sample data.

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 A script that takes in a bunch of authors and titles and generates a report to help determine if it's worth reading.
 
-// Google Books? // Good Reads? // NYTimes?
 ```bash
-pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+pip install --upgrade pynytimes
+
+export NYT_TOKEN=""
 ```
--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 from __future__ import print_function
 
 import os
-from googleapiclient.discovery import build
+import json
+from pynytimes import NYTAPI
 
-GOOGLE_API_TOKEN = os.getenv("GOOGLE_API_TOKEN", None)
+NYT_TOKEN = os.getenv("NYT_TOKEN", None)
 
+books_data = {}
 
 def main():
-    print("TOKEN", GOOGLE_API_TOKEN)
-    books_service = build("books", "v1", developerKey=GOOGLE_API_TOKEN)
-    # print(books_service.bookshelves)
-    print(dir(books_service.bookshelves))
-    # print(books_service.mylibrary)
-    print(dir(books_service.mylibrary))
-    print(dir(books_service))
+    print("NYT Token", NYT_TOKEN)
+    nyt = NYTAPI(NYT_TOKEN)
+
+    authors = {}
+    with open("books.json", "r") as f:
+        authors = json.loads(f.read())
+
+    for author in authors:
+        books = authors[author]
+        for book in books:
+            reviews = nyt.book_reviews(title=book)
+            print(author, book, reviews)
 
 
 if __name__ == '__main__':
     main()
+
+
+"""
+book_dir = "/Users/neil/Downloads/books"
+authors = os.listdir(book_dir)
+
+for author in authors:
+    author_dir = f"{book_dir}/{author}"
+    if os.path.isdir(author_dir):
+        raw_books = os.listdir(author_dir)
+        books = []
+        for raw_book in raw_books:
+            split_book = raw_book.split(" ")
+            print(split_book)
+            book = " ".join(split_book[:-1])
+            books.append(book)
+
+        books_data[author] = books
+
+with open("books.json", "w") as f:
+    f.write(json.dumps(books_data, indent=4))
+"""
